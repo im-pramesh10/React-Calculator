3687af76a54a8e7cf26e019cde96a2c6283b17eb
Pramesh Mahat <cruisepramesh@gmail.com>
2023-01-17 21:46:29 +0545

    calculator skeleton

---
+++ src/components/Display.js
@@ -0,0 +1,20 @@
+import "./Display.css" 
+
+const Display = () => {
+    return(
+        <div className="Display-wrapper">
+            <div className="result">
+                <h1>
+                    4
+                </h1>
+            </div>
+            <div className="expression">
+                <h3>
+                    2+2
+                </h3>
+            </div>
+        </div>
+    );
+}
+
+export default Display;
