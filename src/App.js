a0f9dc922642ced8995a870cb055163b83eed37e
Pramesh Mahat <cruisepramesh@gmail.com>
2023-01-18 01:01:49 +0545

    working calculator

--- src/App.js
+++ src/App.js
@@ -9,11 +9,22 @@ import Display from "./components/Display";
   const [expression, setExpression] = useState("");
 
   const addToExp = (val) => {
-    setExpression(prevExpression => prevExpression + val);
+    const operators = ["+", "-", "*", "/","."];
+    if (operators.indexOf(expression[expression.length-1]) !== -1 && operators.indexOf(val) !== -1) {
+        setExpression(expression);
+    } else {  
+        setExpression(prevExpression => prevExpression + val);
+    }
   }
   const clearDisplay = () => {
     setResult("");
     setExpression("");
+  }
+  const showResult = () => {
+    setResult(parseFloat(eval(expression).toFixed(4)));
+  }
+  const del = () => {
+    setExpression(expression.slice(0,-1));
   }
   
   return (
@@ -24,27 +35,30 @@ import Display from "./components/Display";
           <Button symbol="7" handleClick={ addToExp }/>
           <Button symbol="8" handleClick={ addToExp }/>
           <Button symbol="9" handleClick={ addToExp }/>
-          <Button symbol="+" handleClick={ addToExp }/>
+          <Button symbol="+" bgcolor="grey" color="white" handleClick={ addToExp }/>
         </div>
         <div className="row">
           <Button symbol="4" handleClick={ addToExp }/>
           <Button symbol="5" handleClick={ addToExp }/>
           <Button symbol="6" handleClick={ addToExp }/>
-          <Button symbol="-" handleClick={ addToExp }/>
+          <Button symbol="-" bgcolor="grey" color="white" handleClick={ addToExp }/>
         </div>
         <div className="row">
           <Button symbol="1" handleClick={ addToExp }/>
           <Button symbol="2" handleClick={ addToExp }/>
           <Button symbol="3" handleClick={ addToExp }/>
-          <Button symbol="/" handleClick={ addToExp }/>
+          <Button symbol="/" bgcolor="grey" color="white" handleClick={ addToExp }/>
         </div>
         <div className="row">
           <Button symbol="." handleClick={ addToExp }/>
           <Button symbol="0" handleClick={ addToExp }/>
-          <Button symbol="AC" color="red" handleClick={ clearDisplay }/>
-          <Button symbol="*" handleClick={ addToExp }/>
+          <Button symbol="AC" bgcolor="red" color="white" handleClick={ clearDisplay }/>
+          <Button symbol="*" bgcolor="grey" color="white" handleClick={ addToExp }/>
         </div>
-        <div className="row"><Button symbol="=" color="orange" /></div>
+        <div className="row">
+        <Button symbol="Del" handleClick={ del } color="white" bgcolor="red" />
+        <Button symbol="=" handleClick={ showResult } color="white" bgcolor="orange" />
+        </div>
       </div>
     </div> 
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import "./Button.css";
 
-const Button = ({ symbol, color, handleClick }) => {
+const Button = ({ symbol, bgcolor, color, handleClick }) => {
     return (
         <button 
         className="button-wrapper" 
-        style={{ backgroundColor: color }}
+        style={{ backgroundColor: bgcolor, color: color }}
         onClick={() => handleClick(symbol)}
         >
             { symbol }
